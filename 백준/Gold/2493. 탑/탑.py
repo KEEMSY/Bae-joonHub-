c7b604864044e999a21129cba916e591096ea90e
SeongYeon <96563125+KEEMSY@users.noreply.github.com>
2022-04-17 20:50:50 +0900

    [Gold V] Title: 탑, Time: 864 ms, Memory: 137320 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+towers = list(map(int,input().split()))
+answer = []
+stack = []
+
+for i in range(n):
+    while stack:
+        if stack[-1][1] > towers[i]:
+            answer.append(stack[-1][0]+1)
+            break
+        else:
+            stack.pop()
+    if not stack:
+        answer.append(0)
+    stack.append([i,towers[i]])
+    
+print(*answer)
