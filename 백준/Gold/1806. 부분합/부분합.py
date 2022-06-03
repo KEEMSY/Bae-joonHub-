8f3692fdc365309d03a8970c5539b4fae9ea27a7
SeongYeon <96563125+KEEMSY@users.noreply.github.com>
2022-06-03 21:30:48 +0900

    [Gold IV] Title: 부분합, Time: 172 ms, Memory: 42132 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -2,12 +2,11 @@ n, s = map(int, input().split())
 data = list(map(int,input().split()))
 
 start, end = 0, 0
-min_len = n + 1
-
 temp = data[0]
+min_len = n+1
 temp_len = 1
 
-while end < n:
+while start < n and end < n:
     if temp < s:
         end += 1
         if end == n:
@@ -17,13 +16,13 @@ while end < n:
     else:
         min_len = min(min_len, temp_len)
         temp -= data[start]
-        temp_len -= 1
         start += 1
+        temp_len -= 1
         if start > end:
             end += 1
             if end == n:
                 break
             temp += data[end]
             temp_len += 1
-
+        
 print(0 if min_len == n + 1 else min_len)
