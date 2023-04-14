bcd7b22c3af9f76eb7481a0ce9faeb61e5c3df04
SeongYeon <96563125+KEEMSY@users.noreply.github.com>
2023-04-14 10:27:18 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 27.00 ms, Memory: 12.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -0,0 +1,18 @@
+def solution(cap, n, deliveries, pickups):
+    answer = 0
+    deliveries = deliveries[::-1]
+    pickups = pickups[::-1]
+    
+    now_deli = 0
+    now_pick = 0
+    
+    for i in range(n):
+        now_deli += deliveries[i]
+        now_pick += pickups[i]
+        
+        while now_deli > 0 or now_pick > 0:
+            now_deli -= cap
+            now_pick -= cap
+            answer += (n-i) * 2
+            
+    return answer
