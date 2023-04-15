7b021c5ccbca5ad23da70d19f2962911afb54225
SeongYeon <96563125+KEEMSY@users.noreply.github.com>
2023-04-15 12:13:17 +0900

    [level 3] Title: 입국심사, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -0,0 +1,17 @@
+def solution(n, times):
+    left = 1
+    right = max(times) * n
+    
+    while left < right:
+        mid = (left + right) // 2
+        total = 0
+        for time in times:
+            total += mid // time
+            if total >= n:
+                break
+        if total >= n:
+            right = mid
+        else:
+            left = mid + 1
+            
+    return left
