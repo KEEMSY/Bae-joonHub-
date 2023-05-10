62ae888ce8df730ac008ff5518ce825537df8b02
SeongYeon <96563125+KEEMSY@users.noreply.github.com>
2023-05-10 19:47:04 +0900

    [level 2] Title: 올바른 괄호, Time: 6.91 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public boolean solution(String s) {
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                count++;
+            } else {
+                count--;
+            }
+            if (count < 0) {
+                return false;
+            }
+        }
+        return count == 0;
+    }
+}
