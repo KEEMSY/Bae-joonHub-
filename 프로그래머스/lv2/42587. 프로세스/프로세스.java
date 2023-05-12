2cc6e53047c2cb66f105c107726b0305b082fba4
SeongYeon <96563125+KEEMSY@users.noreply.github.com>
2023-05-12 20:35:53 +0900

    [level 2] Title: 프로세스, Time: 0.79 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int priority:priorities) {
+            pq.add(priority);
+        }
+
+        while( !pq.isEmpty()) {
+            for (int i=0; i<priorities.length; i++) {
+                if( priorities[i] == pq.peek() ) {
+                    if( location == i) {
+                        answer ++;
+                        return answer;
+                    }
+                    pq.poll();
+                    answer ++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
