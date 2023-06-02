6fef5d92985d238d01b6f86f3b8b93861d32c347
SeongYeon <96563125+KEEMSY@users.noreply.github.com>
2023-06-02 19:49:32 +0900

    [level 2] Title: 가장 큰 수, Time: 5.21 ms, Memory: 69.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] nums = Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .toArray(String[]::new);
+
+        Arrays.sort(nums, (a, b) -> (b + a).compareTo(a + b));
+
+        if (nums[0].equals("0")) {
+            return "0";
+        }
+
+        return String.join("", nums);
+    }
+}
