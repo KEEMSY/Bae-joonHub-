c23dbcb943405a95e43a6185fdd8974ded39cf8c
SeongYeon <96563125+KEEMSY@users.noreply.github.com>
2023-06-06 13:37:08 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 1.60 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,46 +1,21 @@
-import java.util.HashMap;
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
+        int[] answer;
+        
         int totalToday = getTotalDays(today);
         
-        HashMap<String, Integer> termsMap = createTermsMap(terms);
-        ArrayList<Integer> answerList = findPrivacyExceeded(totalToday, privacies, termsMap);
-        
-        return convertToArray(answerList);
-    }
-    
-    private int getTotalDays(String today) {
-        String[] todaySplit = today.split("\\.");
-        int year = Integer.parseInt(todaySplit[0]);
-        int month = Integer.parseInt(todaySplit[1]);
-        int day = Integer.parseInt(todaySplit[2]);
-        
-        return year * 12 * 28 + month * 28 + day;
-    }
-    
-    private HashMap<String, Integer> createTermsMap(String[] terms) {
         HashMap<String, Integer> termsMap = new HashMap<>();
-        
         for (String term : terms) {
             String[] termSplit = term.split(" ");
-            String termKey = termSplit[0];
-            int termValue = Integer.parseInt(termSplit[1]) * 28;
-            
-            termsMap.put(termKey, termValue);
+            termsMap.put(termSplit[0], Integer.parseInt(termSplit[1]) * 28);
         }
         
-        return termsMap;
-    }
-    
-    private ArrayList<Integer> findPrivacyExceeded(int totalToday, String[] privacies, HashMap<String, Integer> termsMap) {
-        ArrayList<Integer> answerList = new ArrayList<>();
-        
+        List<Integer> answerList = new ArrayList<>();
         for (int i = 0; i < privacies.length; i++) {
             String[] privacy = privacies[i].split(" ");
-            String privacyKey = privacy[0];
-            int tempTotalDays = getTotalDays(privacyKey);
+            int tempTotalDays = getTotalDays(privacy[0]);
             tempTotalDays += termsMap.get(privacy[1]);
             
             if (tempTotalDays <= totalToday) {
@@ -48,16 +23,22 @@ class Solution {
             }
         }
         
-        return answerList;
-    }
-    
-    private int[] convertToArray(ArrayList<Integer> answerList) {
-        int[] answer = new int[answerList.size()];
-        
+        answer = new int[answerList.size()];
         for (int i = 0; i < answerList.size(); i++) {
             answer[i] = answerList.get(i);
         }
         
         return answer;
+    }
+    
+    private int getTotalDays(String today) {
+        int totalDays = 0;
+        
+        String[] todaySplit = today.split("\\.");
+        totalDays += Integer.parseInt(todaySplit[0]) * 12 * 28;
+        totalDays += Integer.parseInt(todaySplit[1]) * 28;
+        totalDays += Integer.parseInt(todaySplit[2]);
+        
+        return totalDays;
     }
 }
