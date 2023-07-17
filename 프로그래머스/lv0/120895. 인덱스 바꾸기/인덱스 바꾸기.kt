2ce943c270c0cddb2ad615c121fef76c55117275
SeongYeon <96563125+KEEMSY@users.noreply.github.com>
2023-07-17 21:11:04 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 22.97 ms, Memory: 62.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(my_string: String, num1: Int, num2: Int): String {
+        val answer = my_string.toCharArray()
+        answer[num1] = answer[num2].also { answer[num2] = answer[num1] }
+        
+        return answer.joinToString("")
+    }
+}
