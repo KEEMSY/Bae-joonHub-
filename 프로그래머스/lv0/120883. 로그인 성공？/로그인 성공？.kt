23fa68e8a104ad8a55fcb97ce7b81223adf9fcca
SeongYeon <96563125+KEEMSY@users.noreply.github.com>
2023-07-21 18:01:04 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.kt
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.kt
@@ -1,16 +1,7 @@
 class Solution {
     fun solution(id_pw: Array<String>, db: Array<Array<String>>): String {
-        val targetId = id_pw[0]
-        val targetPassword = id_pw[1]
-
-        for (userData in db) {
-            if (userData[0] == targetId && userData[1] == targetPassword) {
-                return "login"
-            } else if (userData[0] == targetId) {
-                return "wrong pw"
-            }
-        }
-
-        return "fail"
+        val user = db.find { it[0] == id_pw[0] } ?: return "fail"
+        if ( user[1] == id_pw[1] ) return "login"
+        return "wrong pw"
     }
 }
