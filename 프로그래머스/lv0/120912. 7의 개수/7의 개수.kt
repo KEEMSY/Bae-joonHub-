0885d574ab75fb35ea34004c0702110c8c928e3b
SeongYeon <96563125+KEEMSY@users.noreply.github.com>
2023-07-24 12:02:02 +0900

    [level 0] Title: 7의 개수, Time: 0.03 ms, Memory: 59.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.kt
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.kt
@@ -1,9 +1,4 @@
 class Solution {
-    fun solution(array: IntArray): Int {
-        var answer: Int = 0
-        for (num in array) {
-            answer += num.toString().count { it == '7' }
-        }
-        return answer
+    fun solution(array: IntArray): Int = array.sumOf { it.toString().count { string -> string == '7' } 
     }
 }
