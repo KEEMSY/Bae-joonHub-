c13f6825efb2122b5058d172c5b0da41ef25a54e
SeongYeon <96563125+KEEMSY@users.noreply.github.com>
2024-03-17 23:13:40 +0900

    [Silver III] Title: 1로 만들기, Time: 132 ms, Memory: 16872 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val n = readLine()!!.toInt()
+    val dp = IntArray(n + 1) { Int.MAX_VALUE }
+    dp[1] = 0 
+
+    for (i in 2..n) {
+        if (i % 3 == 0) dp[i] = minOf(dp[i], dp[i / 3] + 1) 
+        if (i % 2 == 0) dp[i] = minOf(dp[i], dp[i / 2] + 1)
+        dp[i] = minOf(dp[i], dp[i - 1] + 1) 
+    }
+
+    println(dp[n])
+}
